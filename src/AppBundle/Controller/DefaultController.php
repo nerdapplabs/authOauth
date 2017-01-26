5f05f5d7f8f8bc96c6980eca82494bd00b381df2
Praveen Garg <praveen.garg@nerdapplabs.com>
2017-01-27 00:52:54 +0530

    to get stuff cleaned! and to merge PR

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,17 +13,12 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-      $theme = $this->container->getParameter('frontend_theme');
-      $base_dir = realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR;
+         // replace this example code with whatever you need
 
-      $dir    = $base_dir.'/web/'.$theme.'/css';
-      $cssFiles = scandir($dir);
-      $dir    = $base_dir.'/web/'.$theme.'/js';
-      $jsFiles = scandir($dir);
-
-        // replace this example code with whatever you need
         return $this->render('default/index.html.twig', array(
-            'base_dir' => $base_dir, 'theme' => $theme, 'cssFiles' => $cssFiles, 'jsFiles' => $jsFiles
+
+            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+
         ));
     }
 }
