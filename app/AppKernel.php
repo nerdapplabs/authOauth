9856b86b8b5955422b988c598c9f485496fb7784
Praveen Garg <praveen.garg@nerdapplabs.com>
2017-01-27 15:13:57 +0530

    added skeleton theme; using assetic bundle - http://symfony.com/doc/current/assetic/asset_management.html

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -28,6 +28,7 @@ class AppKernel extends Kernel
             new FOS\OAuthServerBundle\FOSOAuthServerBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             // ...
             new AppBundle\AppBundle(),
             new ApiBundle\ApiBundle()
